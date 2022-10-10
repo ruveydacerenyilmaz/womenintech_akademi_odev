96c19f1d84db2949db3a7776fdc159ef6b084b46
ruveydacerenyilmaz <ruveydacerenyilmaz@gmail.com>
2022-10-10 12:20:23 +0300

    Düzenlemeler yapıldı.

--- random.py
+++ random.py
@@ -1,12 +1,14 @@
 import random
-num=random.randint(0,100) #randint ikinci parametreyi dahil ederken, randrange dahil etmez.
-guess=int(input("Sayıyı tahmin ediniz."))
-while guess!=num:
-    if guess>num:
+
+num = random.randint(0,100) #randint ikinci parametreyi dahil ederken, randrange dahil etmez.
+guess = int(input("Sayıyı tahmin ediniz."))
+
+while guess != num:
+    if guess > num:
         print("Daha küçük bir sayı giriniz!")
-        guess=int(input("Tekrar tahmin et:"))
-    if guess<num:
+        guess = int(input("Tekrar tahmin et:"))
+    if guess < num:
         print("Daha büyük bir sayı giriniz!")
-        guess=int(input("Tekrar tahmin et:"))
-    if guess==num:
+        guess = int(input("Tekrar tahmin et:"))
+    if guess == num:
         print("Tebrikler!Doğru bildiniz!")
